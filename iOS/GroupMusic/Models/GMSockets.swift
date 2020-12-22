7959866a2c3cb829b2c07c16fab57d21d0b7a6ba
Your Name <you@example.com>
2020-12-22 03:24:09 -0500

    Test socket connections works

--- iOS/GroupMusic/Models/GMSockets.swift
+++ iOS/GroupMusic/Models/GMSockets.swift
@@ -6,54 +6,21 @@
 //
 
 import Foundation
+import SocketIO
 
 class GMSockets {
     let queuePlayer: GMQueuePlayer
-    private var socketConnection: URLSessionWebSocketTask?
+    private var manager: SocketManager = SocketManager(socketURL: URL(string: "ws://localhost:4400")!, config: [.log(false), .compress])
+    private var socket: SocketIOClient
     
     init(queuePlayer: GMQueuePlayer) {
         self.queuePlayer = queuePlayer
-        
-        self.connectToSocket()
-    }
-    
-    func connectToSocket() {
-        let url = URL(string: "wss://echo.websocket.org")!
-        self.socketConnection = URLSession.shared.webSocketTask(with: url)
-    
-        self.setRecieveHandler()
-        self.socketConnection?.resume()
-        self.sendTestMessage()
-    }
-    
-    private func setRecieveHandler() {
-        self.socketConnection?.receive { (result) in
-            defer { self.setRecieveHandler() }
-            
-            switch result {
-
-            case .success(let message):
-                switch message {
-                case .data(let data):
-                    print("Data recieved \(data)")
-                case .string(let string):
-                    print("String recieved \(string)")
-                @unknown default:
-                    print("Unknown datatype recieved")
-                }
-            case .failure(let error):
-                print("Error in recieving message \(error)")
-            }
+        self.socket = self.manager.defaultSocket
+        self.socket.on(clientEvent: .connect) { (data, ack) in
+            print("Socket status: \(self.socket.status)")
+            self.socket.emit("testEvent", "Hello")
         }
-    }
-    
-    private func sendTestMessage() {
-        let message = URLSessionWebSocketTask.Message.string("Hello World")
-        self.socketConnection?.send(message) { error in
-          if let error = error {
-            print("WebSocket couldn’t send message because: \(error)")
-          }
-        }
+        self.socket.connect()
     }
     
 }
