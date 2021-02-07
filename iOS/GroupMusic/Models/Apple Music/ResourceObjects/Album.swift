06800d0de6ad74f159e9f298f009d50d6e8e13c8
Louis Mark <spideymans.dev@gmail.com>
2021-02-07 12:57:25 -0500

    All AlbumAttributes are optional

--- iOS/GroupMusic/Models/Apple Music/ResourceObjects/Album.swift
+++ iOS/GroupMusic/Models/Apple Music/ResourceObjects/Album.swift
@@ -11,19 +11,19 @@ import Foundation
 public typealias Album = Resource<AlbumAttributes, AlbumRelationships>
 
 public struct AlbumAttributes: Codable {
-    public let artistName: String
-    public let artwork: Artwork
+    public let artistName: String?
+    public let artwork: Artwork?
     public let contentRating: ContentRating?
-    public let copyright: String
+    public let copyright: String?
     public let editorialNotes: EditorialNotes?
-    public let genreNames: [String]
-    public let isComplete: Bool
-    public let isSingle: Bool
-    public let name: String
-    public let releaseDate: String
+    public let genreNames: [String]?
+    public let isComplete: Bool?
+    public let isSingle: Bool?
+    public let name: String?
+    public let releaseDate: String?
     public let playParams: PlayParameters?
-    public let trackCount: Int
-    public let url: URL
+    public let trackCount: Int?
+    public let url: URL?
 }
 
 public struct AlbumRelationships: Codable {
