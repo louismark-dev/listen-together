01ba658113a2368a86a531efcea156b2350774e4
Louis Mark <spideymans.dev@gmail.com>
2021-08-17 19:03:25 -0400

    Removed the custom initializer for the CompactUIViewModel

--- iOS/GroupMusic/UIKit/CompactUI/CompactUIViewModel.swift
+++ iOS/GroupMusic/UIKit/CompactUI/CompactUIViewModel.swift
@@ -12,12 +12,6 @@ class CompactUIViewModel: ObservableObject {
     @Published var isOpen = false
     @Published var layoutData: LayoutData?
     
-    init() {
-        self.layoutData = LayoutData(heading: Text("Return to Now Playing"),
-                                     subheading: Text("Blueberry Faygo - Lil Mosey"),
-                                     leftIcon: Image.ui.arrow_uturn_backward_circle_fill)
-    }
-    
     func open(with layoutData: LayoutData) {
         self.isOpen = true
         self.layoutData = layoutData
